4db7e04eb632f6db30ec9c4aec59e682ae925ba5
alex-dmtr <alexadrian.dmtr@gmail.com>
2016-12-06 16:02:44 +0200

    Add Point class

---
+++ src/Point.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Alex on 06.12.2016.
+ */
+public class Point {
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,40 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * Created by Alex on 06.12.2016.
  */
 public class Test {
 
+    static ArrayList<Point> points;
+    static int[][] a;
+
+    static void back(int k) {
+        if (k == points.size())     
+    }
     public static void main(String[] args) throws FileNotFoundException {
+        int n, m;
 
         Scanner sc = new Scanner(new File("input.txt"));
 
-        while (sc.hasNextLine()) {
-            System.out.printf("%s\n", sc.nextLine());
-        }
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        a = new int[n][m];
+        points = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+
+                if (a[i][j] == 1)
+                    points.add(new Point(i, j));
+            }
+
+
+
 
     }
 }
